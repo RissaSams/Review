428924445480fcbae9e8aa692db38ce538518427
RissaSams <rissa.sams@yahoo.com>
2020-05-27 10:57:16 -0500

    Add project files.

---
+++ Question2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Review
+{
+    class Question2
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        private static void One()
+        {
+            for (int i = 13; i <= 76; i++)
+            {
+                Console.WriteLine($"The loop index is = {i}.");
+            }
+        }
+
+
+        private static void Two()
+        {
+            for (int i = 0; i <= 50; i+=7)
+            {
+                Console.WriteLine($"The loop index squared is: {i}.");
+            }
+        }
+
+        private static void Three()
+        {
+            for (int i = 21; i <= 43; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine($"{i} is an odd number.");
+                }
+                else if (i % 2 == 0)
+                {
+                    Console.WriteLine($"The number {i} is divisible by 2.");
+                }
+            }
+        }
+
+        
+    }
+}
---
+++ Question3.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Review
+{
+    class Question3
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 7, 6, 3, 3, 6, 76, 3, 6, 8, 6, 3, 243, 5, 7 };
+            //  Convert this string into an array of words
+            string line = "Convert this string into an array of words";
+            string[] arr = line.Split(" ");
+
+
+            var largestNum = numbers.Max();
+            Console.WriteLine($"The largest number is {largestNum}.");
+
+            var anyWithG = arr.Any(s => s.EndsWith('g'));
+            Console.WriteLine($"Does any of the words end with a 'g'? {anyWithG}");
+
+            var above20 = numbers.Any(n => n > 20);
+            Console.WriteLine($"Are any number greater than 20? {above20}");
+
+            var sum = numbers.Sum();
+            Console.WriteLine($"The sum of all the numbers is {sum}.");
+
+            var sumBelow50 = numbers.Where(n => n < 50).Sum();
+            Console.WriteLine($"The sum of the numbers below 50 is {sumBelow50}.");
+
+
+            var middle = numbers.ElementAt(numbers.Length / 2);
+            Console.WriteLine($"{middle} is the number in the middle of the array.");
+
+            var mid = arr.ElementAt(arr.Length / 2);
+            Console.WriteLine($"{mid} is the word in the middle of the array.");
+
+            var first4 = numbers.Take(4);
+
+            var middle3Words = arr.Skip(3).Take(3);
+
+            var ave = numbers.Average();
+            Console.WriteLine($"The average is {ave}.");
+        }
+    }
+}
